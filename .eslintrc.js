ea1d837204162cdf4e4a007852f3bfb6e23c0f4f
Shivam <kshivam1603@gmail.com>
2021-03-14 18:54:08 +0530

    first

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': 0,
+    'prefer-const': 0,
+  },
+};
